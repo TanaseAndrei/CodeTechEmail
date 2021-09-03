0b92fb43a2bfcd359ffb849e9e510bcd6fa4a868
Tanase Andrei Cristi <stonixandrei@yahoo.com>
2021-09-04 00:58:59 +0300

    Added validation exception to display the errors from the fields

--- src/main/java/com/ucv/codetechemail/controller/exception/converter/AppExceptionConverter.java
+++ src/main/java/com/ucv/codetechemail/controller/exception/converter/AppExceptionConverter.java
@@ -1,11 +1,18 @@
 package com.ucv.codetechemail.controller.exception.converter;
 
 import com.ucv.codetechemail.controller.exception.AppException;
+import com.ucv.codetechemail.controller.exception.dto.ValidationExceptionDto;
 import com.ucv.codetechemail.controller.exception.dto.AppExceptionDto;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @NoArgsConstructor
 @Component
@@ -19,5 +26,24 @@ public class AppExceptionConverter {
         appExceptionDto.setMessage(appException.getMessage());
         appExceptionDto.setThrownTime(LocalDateTime.now());
         return appExceptionDto;
+    }
+
+    public ValidationExceptionDto validationExceptionToDto(MethodArgumentNotValidException methodArgumentNotValidException) {
+        ValidationExceptionDto validationExceptionDto = new ValidationExceptionDto();
+        validationExceptionDto.setHttpCode(HttpStatus.BAD_REQUEST.value());
+        validationExceptionDto.setMessage(VALIDATION_EXCEPTION_MESSAGE);
+        Map<String, String> fieldsWithErrors = new LinkedHashMap<>();
+        methodArgumentNotValidException.getBindingResult().getAllErrors().forEach(objectError -> {
+            populateMapWithError(fieldsWithErrors, objectError);
+        });
+        validationExceptionDto.setFieldsWithErrors(fieldsWithErrors);
+        validationExceptionDto.setThrownTime(LocalDateTime.now());
+        return validationExceptionDto;
+    }
+
+    private void populateMapWithError(Map<String, String> fieldsWithErrors, ObjectError objectError) {
+        String fieldName = ((FieldError) objectError).getField();
+        String errorMessage = objectError.getDefaultMessage();
+        fieldsWithErrors.put(fieldName, errorMessage);
     }
 }
--- src/main/java/com/ucv/codetechemail/controller/model/input/RegisterEmailDto.java
+++ src/main/java/com/ucv/codetechemail/controller/model/input/RegisterEmailDto.java
@@ -6,21 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
-@ApiModel(description = "This represents the object that the api needs to get in order to know to who must be sent the email," +
-        " to what email and what role does the user have")
+@ApiModel(description = "The object that the API needs to get in order to know to who must be sent the email," +
+        " to what email and what role does the user have to find the specific template")
 public class RegisterEmailDto {
 
     @NotEmpty(message = "The username should not be null or empty")
     @ApiModelProperty(required = true, value = "The name of the user", example = "User1")
     private final String username;
 
-    @NotEmpty(message = "The email should not be empty")
+    @NotEmpty(message = "The email should not be null or empty")
     @ApiModelProperty(required = true, value = "The email address where the actual email should be sent", example = "a@yahoo.com")
     private final String to;
 
+    @NotNull(message = "The role of the user should be specified")
     @ApiModelProperty(required = true, value = "The role of the user", example = "STUDENT")
     private final RoleDto role;
 }
